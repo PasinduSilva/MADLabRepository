a7e360d8321c38c8b8239aee7122ad0f06ea95a5
PasinduSilva <86094672+PasinduSilva@users.noreply.github.com>
2021-08-20 14:02:17 +0530

    Additional Lab Sheet

---
+++ app/src/main/java/com/example/androidservices/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.androidservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MediaPlayer player = MediaPlayer.create(this, Settings.System.DEFAULT_NOTIFICATION_URI);
+        player.setLooping(true); player.start();
+
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+        Button btnStop = (Button) findViewById(R.id.btnStop);
+        
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+    }
+}
